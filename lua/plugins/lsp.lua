e2d957c5358898052c229f76f5b1437419f89a67
agrigoryan <aram.grigoryan@gmail.com>
2023-12-15 01:50:04 -0500

    undotree, lsp updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,5 +42,26 @@ return {
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
       vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
     end)
+
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      })
+    })
   end
 }
